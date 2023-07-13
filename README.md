155bd68e5287f9cda84579cb60ba41f591b8edb4
shubhax <shubhadeep161@gmail.com>
2023-07-13 22:17:33 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# 🔄 chfe - Change Files Extensions
+
+[![npm](https://img.shields.io/badge/npm-v7.0.0-red.svg)](https://www.npmjs.com/package/chfe)
+[![Node.js](https://img.shields.io/badge/node-v14.0.0-green.svg)](https://nodejs.org/)
+[![License](https://img.shields.io/badge/license-ISC-blue.svg)](LICENSE)
+
+
+## Description
+
+chfe (Change Files Extensions) is a command-line tool that simplifies the process of changing file extensions within a specified folder. It allows you to quickly and effortlessly update the extensions of multiple files in one go. Whether you're migrating to a different file format or organizing your files, chfe automates the renaming process, saving you time and effort.
+
+## Features
+
+- 🔄 Change file extensions in a specified folder
+- 🚀 Batch rename multiple files at once
+- 🌟 Simple and intuitive command-line interface
+- ⚡️ Built-in file management with Node.js
+- 🔄 Easily convert file formats with ease
+
+## Installation
+
+To install chfe, simply run the following command:
+
+```bash
+npm install -g chfe
+```
+
+## Usage Examples
+
+### Change file extensions in the "source" folder from .js to .mjs
+
+```bash
+chfe -f source -ce js -ne mjs
+```
+
+### Display help and options
+
+```bash
+chfe -h
+```
+
+## Options
+
+- `-h, --help`: Display help and usage instructions.
+- `-f, --folder <folder>`: Specify the folder name to change file extensions.
+- `-ce, --current-extension <current_extension>`: Specify the current file extension.
+- `-ne, --new-extension <new_extension>`: Specify the new file extension.
+
+## License
+
+This project is licensed under the ISC License. See the [LICENSE](LICENSE) file for details.
+
+Feel free to customize and enhance the README file to suit your specific requirements and project details.
---
+++ index.js
@@ -0,0 +1,96 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
+function changeFileExtensions(folderPath, currentExtension, newExtension) {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error("Error reading folder:", err);
+      return;
+    }
+
+    files.forEach((file) => {
+      const filePath = path.join(folderPath, file);
+
+      if (path.extname(filePath) === `.${currentExtension}`) {
+        const newFilePath = path.join(
+          folderPath,
+          path.basename(file, `.${currentExtension}`) + `.${newExtension}`
+        );
+
+        fs.rename(filePath, newFilePath, (err) => {
+          if (err) {
+            console.error(`Error renaming file '${file}':`, err);
+          } else {
+            console.log(
+              `** Renamed '${file}' to '${path.basename(newFilePath)}'`
+            );
+          }
+        });
+      }
+    });
+  });
+}
+
+function displayHelp() {
+  console.log(
+    "Usage: chfe -f <folder> -ce <current_extension> -ne <new_extension>"
+  );
+  console.log("Options:");
+  console.log("  -h, --help                    Display this help message");
+  console.log("  -f, --folder <folder>         Specify the folder name");
+  console.log("  -ce, --current-extension <current_extension>");
+  console.log(
+    "                                Specify the current file extension"
+  );
+  console.log("  -ne, --new-extension <new_extension>");
+  console.log("                                Specify the new file extension");
+}
+
+const args = process.argv.slice(2);
+
+if (args.includes("-h") || args.includes("--help")) {
+  displayHelp();
+  process.exit(0);
+}
+
+const options = {
+  folderPath: "",
+  currentExtension: "",
+  newExtension: "",
+};
+
+for (let i = 0; i < args.length; i += 2) {
+  const option = args[i];
+  const value = args[i + 1];
+
+  switch (option) {
+    case "-f":
+    case "--folder":
+      options.folderPath = value;
+      break;
+    case "-ce":
+    case "--current-extension":
+      options.currentExtension = value;
+      break;
+    case "-ne":
+    case "--new-extension":
+      options.newExtension = value;
+      break;
+    default:
+      console.error("Invalid option:", option);
+      displayHelp();
+      process.exit(1);
+  }
+}
+
+if (!options.folderPath || !options.currentExtension || !options.newExtension) {
+  console.error("Missing required options.");
+  displayHelp();
+  process.exit(1);
+}
+
+changeFileExtensions(
+  options.folderPath,
+  options.currentExtension,
+  options.newExtension
+);
